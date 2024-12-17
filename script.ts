76503e994013be34a7a01839868a5b3fdf6dd21d
Elisa <elisa.salvatore110@gmail.com>
2024-12-17 16:15:02 +0100

    add: interfaces for vehicle, user and city

--- script.ts
+++ script.ts
@@ -1,1 +1,28 @@
-console.log("test");
+//Types
+type MezzoDiTrasporto = "bicicletta" | "scooter" | "monopattino";
+type MetodoDiPagamento = "carta" | "satispay";
+
+//Interfaces
+interface ICitta {
+	nomeCitta: string;
+	mezziDisponibili: IMezzo[];
+
+	aggiungiMezzo(mezzo: IMezzo): void;
+}
+
+interface IUtente {
+	nome: string;
+	congnome: string;
+	email: string;
+	pagamento: MetodoDiPagamento;
+
+	prenotaMezzo(mezzo: IMezzo): void;
+}
+
+interface IMezzo {
+	tipo: MezzoDiTrasporto;
+	id: number;
+	disponibile: boolean;
+
+	assegnaUtente(utente: IUtente): void;
+}
